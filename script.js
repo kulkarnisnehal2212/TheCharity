dff14a91c6ea64b9468ee5d55c2c23988364c3b7
kulkarnisnehal947 <kulkarnisnehal2212@gmail.com>
2025-12-24 17:22:38 +0530

    changes added 24/12/25

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 let slideIndex = 1;
+let autoSlideInterval;
+
 showSlides(slideIndex);
+startAutoSlide();
 
 function plusSlides(n) {
+    clearInterval(autoSlideInterval);
     showSlides(slideIndex += n);
+    startAutoSlide();
 }
 
 function showSlides(n) {
@@ -12,11 +17,30 @@ function showSlides(n) {
     if (n < 1) slideIndex = slides.length;
 
     for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        slides[i].classList.remove("active");
     }
 
-    slides[slideIndex - 1].style.display = "block";
+    slides[slideIndex - 1].classList.add("active");
 }
+
+function startAutoSlide() {
+    autoSlideInterval = setInterval(() => {
+        slideIndex++;
+        showSlides(slideIndex);
+    }, 3500);
+}
+
+function stopAutoSlide() {
+    clearInterval(autoSlideInterval);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const sliderContainer = document.querySelector('.slider-container');
+    if (sliderContainer) {
+        sliderContainer.addEventListener('mouseenter', stopAutoSlide);
+        sliderContainer.addEventListener('mouseleave', startAutoSlide);
+    }
+});
 
 
 
